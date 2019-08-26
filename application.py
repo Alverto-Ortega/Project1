fd1c043ea61d649cb64fca251e166504bf89af5b
Alverto Ortega <ortega361@yahoo.com>
2019-08-25 20:20:55 -0400

    added more files html and python , also updated code to login and log out after registering

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session,render_template
+from flask import Flask, session,render_template,request,redirect,url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -21,6 +21,46 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/login")    #have to add "login to end of url of server url"
+def login():    
+    return render_template("loginPage.html")
+'''
+@app.route("/registration", methods =["POST"])
+def registration():
+    #get form information
+    name = request.form.get("name")
+    password = request.form.get("password")
+    user = db.execute("INSERT INTO users (name, password) VALUES (:name, :password)", 
+                        {"name": name, "password": password})
+    users = db.execute("SELECT * FROM users").fetchall()
+    db.commit()
+    return render_template("loginPage.html" ,users= users ) #"success.html will later change to loginPage.html after testing is done"
+'''
+@app.route("/success", methods= ["POST"])
+def success():
+    
+    name = request.form.get("name")
+    password = request.form.get("password")
+   
+    if db.execute("SELECT * FROM users WHERE name = :name AND password = :password ",
+                        {"name": name, "password": password }).fetchone() is None:
+        return render_template("error.html")
+    db.commit()
+   
+    return render_template("success.html", name = name )
+
+@app.route("/logout", methods =["POST"])
+def logout():
+    session.pop("login", None)
+    return redirect(url_for('login'))
+
+'''
+@app.route("/logout", methods =["POST"])
+def logout():
+    db.
+    try:
+        userId = int(request.form.get("userId"))
+    except ValueError:
+        return render_template("error.html", message="Invalid user id")
+    return render_template("success.html")
+'''
---
+++ import.py
@@ -0,0 +1,13 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    b = open("books.csv")
+    reader = csv.reader(b)
+    for 
