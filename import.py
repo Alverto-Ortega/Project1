844fbe93241fa25a8a81f517e6f1e5c980fdf444
Alverto Ortega <ortega361@yahoo.com>
2019-09-01 14:19:27 -0400

    added user input to determine whether to add data or to display table data

--- import.py
+++ import.py
@@ -14,20 +14,49 @@ db = scoped_session(sessionmaker(bind=engine))
 def main():
     b = open("books.csv")
     reader = csv.reader(b)
-   
+    
+
+    #later on figure out how to insert into two tables through books table's reference authorid to insert as a authorname value in authours table
+    
+    #asks user input for choices of adding data to book table or displaying current data table contents
+    userInput = input("would you like to add data to your books table? YES or NO: ")
+    if userInput == "YES":
+        addBook(reader)
+        userInput=input("Would you like to display your current book table data? YES or NO: ")
+        if userInput =="YES":
+            displayData()
+        else:
+            print("Thank you, have a great day! ")
+
+    else:
+        userInput =input("Would you like to display your current book table data? YES or NO: ")
+        if userInput =="YES":
+            displayData()
+        else:
+            print("Thank you, have a great day! ")
+
+def addBook(reader):
+    #an if statement for checking if we want to insert new data but not repeating the same insertion of the same previous data
     next(reader) #skips over the first line headings in content so that it doesnt store the headings into the database
     for isbnnumber, title,authorname,publicyear, in reader:
         db.execute("INSERT INTO books(isbnnumber,title,authorname,publicyear) VALUES (:isbnnumber, :title,  :authorname, :publicyear)",
-                    {"isbnnumber": isbnnumber, "title": title,  "authorname": authorname,"publicyear": publicyear})
- 
-        print(f"added book: {title} from {authorname} published in {publicyear} with and isbn number : {isbnnumber}.")
+                    {"isbnnumber": isbnnumber, "title": title,  "authorname": authorname,"publicyear": publicyear}) 
+        #print(f"added book: {title} from {authorname} published in {publicyear} with and isbn number : {isbnnumber}.")
+   
     db.commit()
-def displayData():
-    books = db.execute("SELECT isbnnumber,title, authorname,publicyear FROM books").fetchall()
+    print("You have Successfully inserted all of your requested Data, now the data will be displayed upon request... \n")
 
-    for book in books:
-        print(f" isbn number: {book.isbnnumber} title: {book.title} author Name:{book.authorname} public year:{book.publicyear}")
+def displayData():
+    books = db.execute("SELECT * FROM books").fetchall()
+    print(books)
+    db.commit()
+
+    #for book in books:
+    #   print(f" isbn number: {book.isbnnumber} title: {book.title} author Name:{book.authorname} public year:{book.publicyear}")
+
+
 if __name__ == "__main__":
     main()
-displayData()
+
+
     
